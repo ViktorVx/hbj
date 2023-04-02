9206ce64051ce571f971afc1932e83c54a2f8c21
ViktorVx <victorptrv@yandex.ru>
2023-04-02 20:08:23 +0300

    Init commit

---
+++ src/main/java/org/pva/hbj/HbjApplication.java
@@ -0,0 +1,13 @@
+package org.pva.hbj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HbjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/pva/hbj/HbjApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pva.hbj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HbjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
