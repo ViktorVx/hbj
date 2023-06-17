72f6b3b3429f88cb3bb3b4c51ec5236341f40662
ViktorVx <victorptrv@yandex.ru>
2023-06-17 20:19:05 +0300

    Refactor

--- src/main/java/org/pva/hbj/controller/JourneyBot.java
+++ src/main/java/org/pva/hbj/controller/JourneyBot.java
@@ -4,22 +4,16 @@ import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.pva.hbj.data.Journey;
-import org.pva.hbj.data.Message;
 import org.pva.hbj.data.StoryLevel;
 import org.pva.hbj.provider.ParamsProvider;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.methods.send.SendAudio;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
-import org.telegram.telegrambots.meta.api.methods.send.SendVideo;
-import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import java.io.File;
 import java.util.concurrent.TimeUnit;
+
+import static org.pva.hbj.utils.send.SendMessagesUtil.sendMessage;
 
 @Slf4j
 @Component
@@ -28,8 +22,6 @@ public class JourneyBot extends TelegramLongPollingBot {
 
     private final ParamsProvider paramsProvider;
     private final Journey journey;
-    @Value("${telegram.bot.hbj.media-path}")
-    private String mediaPath;
     @Value("${settings.message-new-level-delay}")
     private Integer newLevelDelay;
 
@@ -58,19 +50,19 @@ public class JourneyBot extends TelegramLongPollingBot {
         }
         if (journey.isLastStoryPage()) {
             if (journey.doWin()) {
-                sendMessage(update, "Ура! Ты победил!");
+                sendMessage(this, update, "Ура! Ты победил!");
                 journey.noneModeOn();
                 return;
             }
             journey.goNextLevel();
             if (journey.getCurrentLevel() instanceof StoryLevel) {
-                sendMessage(update, journey.getLevelStory());
+                sendMessage(this, update, journey.getLevelStory());
             } else {
-                sendMessage(update, journey.getLevelTask());
+                sendMessage(this, update, journey.getLevelTask());
             }
         } else {
             journey.goNextPage();
-            sendMessage(update, journey.getLevelStory());
+            sendMessage(this, update, journey.getLevelStory());
         }
     }
 
@@ -79,17 +71,17 @@ public class JourneyBot extends TelegramLongPollingBot {
             return;
         }
         journey.enterCodeModeOff();
-        sendMessage(update, journey.getLevelTask());
+        sendMessage(this, update, journey.getLevelTask());
     }
 
     private void applyCode(Update update) {
         journey.enterCodeModeOn();
-        sendMessage(update, "Введи секретный код: ");
+        sendMessage(this, update, "Введи секретный код: ");
     }
 
     private void start(Update update) {
         journey.reset();
-        sendMessage(update, journey.getLevelTask());
+        sendMessage(this, update, journey.getLevelTask());
     }
 
     private void process(Update update) throws InterruptedException {
@@ -108,15 +100,15 @@ public class JourneyBot extends TelegramLongPollingBot {
         var msg = success ? "Отлично)! Поехали дальше)!" : "Не, не, не)) ты хочешь меня надурить)!";
         if (success) {
             if (journey.doWin()) {
-                sendMessage(update, "Ура! Ты победил!");
+                sendMessage(this, update, "Ура! Ты победил!");
             } else {
-                sendMessage(update, msg);
+                sendMessage(this, update, msg);
                 journey.goNextLevel();
-                sendMessage(update, journey.getLevelTask());
+                sendMessage(this, update, journey.getLevelTask());
             }
         } else {
-            sendMessage(update, msg);
-            sendMessage(update, journey.getLevelTask());
+            sendMessage(this, update, msg);
+            sendMessage(this, update, journey.getLevelTask());
         }
     }
 
@@ -126,16 +118,16 @@ public class JourneyBot extends TelegramLongPollingBot {
         var msg = success ? "Правильно)! Поехали дальше)!" : "Не верно( Подумай еще!";
         if (success) {
             if (journey.doWin()) {
-                sendMessage(update, "Ура! Ты победил!");
+                sendMessage(this, update, "Ура! Ты победил!");
             } else {
-                sendMessage(update, msg);
+                sendMessage(this, update, msg);
                 journey.goNextLevel();
                 TimeUnit.SECONDS.sleep(newLevelDelay);
-                sendMessage(update, journey.getLevelTask());
+                sendMessage(this, update, journey.getLevelTask());
             }
         } else {
-            sendMessage(update, msg);
-            sendMessage(update, journey.getLevelTask());
+            sendMessage(this, update, msg);
+            sendMessage(this, update, journey.getLevelTask());
         }
     }
 
@@ -143,7 +135,7 @@ public class JourneyBot extends TelegramLongPollingBot {
         journey.storyModeOn();
         if (journey.doWin()) {
             journey.noneModeOn();
-            sendMessage(update, "Ура! Ты победил!");
+            sendMessage(this, update, "Ура! Ты победил!");
         }
     }
 
@@ -153,103 +145,16 @@ public class JourneyBot extends TelegramLongPollingBot {
         if (journey.secretCodeExists(answer)) {
             log.info("Secret code exist");
             journey.moveToLevelBySecretCode(answer);
-            sendMessage(update, "Вжух)! И переносимся на нужный уровень)!");
-            sendMessage(update, journey.getLevelTask());
+            sendMessage(this, update, "Вжух)! И переносимся на нужный уровень)!");
+            sendMessage(this, update, journey.getLevelTask());
             journey.enterCodeModeOff();
         } else {
-            sendMessage(update, "Нету такого кода) ты хочешь меня надурить)");
+            sendMessage(this, update, "Нету такого кода) ты хочешь меня надурить)");
         }
     }
 
     private void processNoneMode() {
         log.info("None mode processing complete");
-    }
-
-    private void sendMessage(Update update, String msg) {
-        sendMessage(update, Message.builder().text(msg).mediaType(null).build());
-    }
-
-    private void sendMessage(Update update, Message msg) {
-        if (msg.getMediaType() == null) {
-            sendTextMessage(update, msg);
-        } else {
-            switch (msg.getMediaType()) {
-                case IMAGE -> sendImageMessage(update, msg);
-                case VIDEO -> sendVideoMessage(update, msg);
-                case AUDIO -> sendAudioMessage(update, msg);
-            }
-        }
-    }
-
-    private void sendAudioMessage(Update update, Message msg) {
-        try {
-            var audio = new InputFile(new File(mediaPath + msg.getMediaPath()), "filename");
-            var message = new SendAudio();
-            message.setChatId(extractChatId(update));
-            message.setCaption(msg.getText());
-            message.setAudio(audio);
-            if (msg.getKeyboard() != null) {
-                message.setReplyMarkup(msg.getKeyboard());
-            }
-            execute(message);
-        } catch (TelegramApiException e) {
-            log.error(e.toString());
-        }
-    }
-
-    private void sendVideoMessage(Update update, Message msg) {
-        try {
-            var video = new InputFile(new File(mediaPath + msg.getMediaPath()), "filename");
-            var message = new SendVideo();
-            message.setChatId(extractChatId(update));
-            message.setCaption(msg.getText());
-            message.setVideo(video);
-            if (msg.getKeyboard() != null) {
-                message.setReplyMarkup(msg.getKeyboard());
-            }
-            execute(message);
-        } catch (TelegramApiException e) {
-            log.error(e.toString());
-        }
-    }
-
-    private void sendImageMessage(Update update, Message msg) {
-        try {
-            var photo = new InputFile(new File(mediaPath + msg.getMediaPath()), "filename");
-            var message = new SendPhoto();
-            message.setChatId(extractChatId(update));
-            message.setCaption(msg.getText());
-            message.setPhoto(photo);
-            if (msg.getKeyboard() != null) {
-                message.setReplyMarkup(msg.getKeyboard());
-            }
-            execute(message);
-        } catch (TelegramApiException e) {
-            log.error(e.toString());
-        }
-    }
-
-    private void sendTextMessage(Update update, Message msg) {
-        try {
-            var message = new SendMessage();
-            message.setChatId(extractChatId(update));
-            message.setText(msg.getText());
-            message.enableHtml(true);
-            if (msg.getKeyboard() != null) {
-                message.setReplyMarkup(msg.getKeyboard());
-            }
-            execute(message);
-        } catch (TelegramApiException e) {
-            log.error(e.toString());
-        }
-    }
-
-    private String extractChatId(Update update) {
-        if (update.hasCallbackQuery()) {
-            return update.getCallbackQuery().getMessage().getChatId().toString();
-        } else {
-            return update.getMessage().getChatId().toString();
-        }
     }
 
     @Override
---
+++ src/main/java/org/pva/hbj/utils/send/SendMessagesUtil.java
@@ -0,0 +1,113 @@
+package org.pva.hbj.utils.send;
+
+import lombok.extern.slf4j.Slf4j;
+import org.pva.hbj.data.Message;
+import org.springframework.beans.factory.annotation.Value;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendAudio;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.methods.send.SendVideo;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.io.File;
+
+@Slf4j
+public class SendMessagesUtil {
+
+    @Value("${telegram.bot.hbj.media-path}")
+    private static String mediaPath;
+
+    private SendMessagesUtil() {
+
+    }
+
+    public static void sendMessage(TelegramLongPollingBot bot, Update update, String msg) {
+        sendMessage(bot, update, Message.builder().text(msg).mediaType(null).build());
+    }
+
+    public static void sendMessage(TelegramLongPollingBot bot, Update update, Message msg) {
+        if (msg.getMediaType() == null) {
+            sendTextMessage(bot, update, msg);
+        } else {
+            switch (msg.getMediaType()) {
+                case IMAGE -> sendImageMessage(bot, update, msg);
+                case VIDEO -> sendVideoMessage(bot, update, msg);
+                case AUDIO -> sendAudioMessage(bot, update, msg);
+            }
+        }
+    }
+
+    private static void sendTextMessage(TelegramLongPollingBot bot, Update update, Message msg) {
+        try {
+            var message = new SendMessage();
+            message.setChatId(extractChatId(update));
+            message.setText(msg.getText());
+            message.enableHtml(true);
+            if (msg.getKeyboard() != null) {
+                message.setReplyMarkup(msg.getKeyboard());
+            }
+            bot.execute(message);
+        } catch (TelegramApiException e) {
+            log.error(e.toString());
+        }
+    }
+
+    private static void sendImageMessage(TelegramLongPollingBot bot, Update update, Message msg) {
+        try {
+            var photo = new InputFile(new File(mediaPath + msg.getMediaPath()), "filename");
+            var message = new SendPhoto();
+            message.setChatId(extractChatId(update));
+            message.setCaption(msg.getText());
+            message.setPhoto(photo);
+            if (msg.getKeyboard() != null) {
+                message.setReplyMarkup(msg.getKeyboard());
+            }
+            bot.execute(message);
+        } catch (TelegramApiException e) {
+            log.error(e.toString());
+        }
+    }
+
+    private static void sendAudioMessage(TelegramLongPollingBot bot, Update update, Message msg) {
+        try {
+            var audio = new InputFile(new File(mediaPath + msg.getMediaPath()), "filename");
+            var message = new SendAudio();
+            message.setChatId(extractChatId(update));
+            message.setCaption(msg.getText());
+            message.setAudio(audio);
+            if (msg.getKeyboard() != null) {
+                message.setReplyMarkup(msg.getKeyboard());
+            }
+            bot.execute(message);
+        } catch (TelegramApiException e) {
+            log.error(e.toString());
+        }
+    }
+
+    private static void sendVideoMessage(TelegramLongPollingBot bot, Update update, Message msg) {
+        try {
+            var video = new InputFile(new File(mediaPath + msg.getMediaPath()), "filename");
+            var message = new SendVideo();
+            message.setChatId(extractChatId(update));
+            message.setCaption(msg.getText());
+            message.setVideo(video);
+            if (msg.getKeyboard() != null) {
+                message.setReplyMarkup(msg.getKeyboard());
+            }
+            bot.execute(message);
+        } catch (TelegramApiException e) {
+            log.error(e.toString());
+        }
+    }
+
+    private static String extractChatId(Update update) {
+        if (update.hasCallbackQuery()) {
+            return update.getCallbackQuery().getMessage().getChatId().toString();
+        } else {
+            return update.getMessage().getChatId().toString();
+        }
+    }
+}
